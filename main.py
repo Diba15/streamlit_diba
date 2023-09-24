8218999a602d507f694aa3664c60454d73a7d40e
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2023-09-24 19:52:56 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title='Home')
+
+st.title('Test Streamlit')
+
+st.write(
+    "Website Test Streamlit, yang bertujuan untuk mempelajari bagaimana streamlit bekerja dan bagaimana cara "
+    "menggunakan nya secara efektif.")
+
+st.header("Test Macam-macam fitur dalam Streamlit", divider='red')
+
+st.write("Disini kita akan mencoba beberapa fitur yang dapat digunakan dalam Streamlit.")
+
+st.subheader("Data Frame")
+
+st.write(pd.DataFrame({
+    'Nama': ["Dimas Bagas", "Test1", "Test2", "Test3"],
+    'NIM': ["1301228515", "1301228512", "1301228513", "1301228511"]
+}))
+
+st.subheader("Penulisan Derajat Temperature")
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+
+st.subheader("Test Forms")
+
+with st.form(key="login_form"):
+    username = st.text_input("Username")
+    password = st.text_input("password", type="password")
+    st.form_submit_button("Submit")
+
