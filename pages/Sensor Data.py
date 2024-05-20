076d9c1d81c2693ce3e2a6bf64c34af6892876e0
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2024-05-20 14:05:52 +0700

    Menambahkan pre processing dan Modelling

--- pages/Sensor Data.py
+++ pages/Sensor Data.py
@@ -6,6 +6,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.graph_objects as go
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
 
 st.set_page_config(page_title='Tugas Skripsi', layout='wide')
 
@@ -452,6 +458,218 @@ def data_sensor():
     plt.yticks(fontsize=10)
 
     st.pyplot(f)
+
+    #     Filtered Data
+    st.header("Filtered Data")
+
+    # add text below the image
+    st.write("Image Source: https://i-qlair.com/indoor-air-quality-monitoring-complete-guide/")
+    st.image("./image/iaqi_metric.png")
+
+    #     Filter just CO2 and TVOC
+    df_filtered = df[["co2", "tvoc"]]
+
+    st.dataframe(df_filtered)
+
+    #     Make a IAQI Calculation for CO2 and TVOC
+    st.header("IAQI Calculation")
+
+    def calculate_iaqi(co2, tvoc):
+
+        if 0 <= co2 <= 500:
+            iaqi_co2 = ((50 - 0) / (500 - 0)) * (co2 - 0) + 0
+        elif 500 < co2 <= 1000:
+            iaqi_co2 = ((100 - 51) / (1000 - 501)) * (co2 - 501) + 51
+        elif 1000 < co2 <= 2000:
+            iaqi_co2 = ((150 - 101) / (2000 - 1001)) * (co2 - 1001) + 101
+        elif 2000 < co2 <= 5000:
+            iaqi_co2 = ((200 - 151) / (5000 - 2001)) * (co2 - 2001) + 151
+        elif 5000 < co2 <= 10000:
+            iaqi_co2 = ((300 - 201) / (10000 - 5001)) * (co2 - 5001) + 201
+        elif 10000 < co2 <= 20000:
+            iaqi_co2 = ((400 - 301) / (20000 - 10001)) * (co2 - 10001) + 301
+        elif 20000 < co2 <= 50000:
+            iaqi_co2 = ((500 - 401) / (50000 - 20001)) * (co2 - 20001) + 401
+        elif 50000 < co2 <= 100000:
+            iaqi_co2 = ((600 - 501) / (100000 - 50001)) * (co2 - 50001) + 501
+        elif 100000 < co2 <= 200000:
+            iaqi_co2 = ((700 - 601) / (200000 - 100001)) * (co2 - 100001) + 601
+        elif 200000 < co2 <= 400000:
+            iaqi_co2 = ((800 - 701) / (400000 - 200001)) * (co2 - 200001) + 701
+        elif 400000 < co2 <= 1000000:
+            iaqi_co2 = ((900 - 801) / (1000000 - 400001)) * (co2 - 400001) + 801
+        else:
+            iaqi_co2 = 1000
+
+        if 0 <= tvoc <= 0.6:
+            iaqi_tvoc = ((50 - 0) / (0.6 - 0)) * (tvoc - 0) + 0
+        elif 0.6 < tvoc <= 1.0:
+            iaqi_tvoc = ((100 - 51) / (1.0 - 0.6)) * (tvoc - 0.6) + 51
+        elif 1.0 < tvoc <= 2.0:
+            iaqi_tvoc = ((150 - 101) / (2.0 - 1.0)) * (tvoc - 1.0) + 101
+        elif 2.0 < tvoc <= 3.0:
+            iaqi_tvoc = ((200 - 151) / (3.0 - 2.0)) * (tvoc - 2.0) + 151
+        elif 3.0 < tvoc <= 5.0:
+            iaqi_tvoc = ((300 - 201) / (5.0 - 3.0)) * (tvoc - 3.0) + 201
+        elif 5.0 < tvoc <= 10.0:
+            iaqi_tvoc = ((400 - 301) / (10.0 - 5.0)) * (tvoc - 5.0) + 301
+        elif 10.0 < tvoc <= 20.0:
+            iaqi_tvoc = ((500 - 401) / (20.0 - 10.0)) * (tvoc - 10.0) + 401
+        elif 20.0 < tvoc <= 30.0:
+            iaqi_tvoc = ((600 - 501) / (30.0 - 20.0)) * (tvoc - 20.0) + 501
+        elif 30.0 < tvoc <= 40.0:
+            iaqi_tvoc = ((700 - 601) / (40.0 - 30.0)) * (tvoc - 30.0) + 601
+        elif 40.0 < tvoc <= 50.0:
+            iaqi_tvoc = ((800 - 701) / (50.0 - 40.0)) * (tvoc - 40.0) + 701
+        elif 50.0 < tvoc <= 100.0:
+            iaqi_tvoc = ((900 - 801) / (100.0 - 50.0)) * (tvoc - 50.0) + 801
+        else:
+            iaqi_tvoc = 1000
+
+        return iaqi_co2, iaqi_tvoc
+
+    st.write("Rumus yang terkandung dalam fungsi calculate_iaqi adalah sebagai berikut:")
+    st.latex(r'IAQI_{CO2} = \frac{I_{H} - I_{L}}{C_{H} - C_{L}} \times (CO2 - C_{L}) + I_{L}')
+    st.latex(r'IAQI_{TVOC} = \frac{I_{H} - I_{L}}{C_{H} - C_{L}} \times (TVOC - C_{L}) + I_{L}')
+
+    # Reference of the formula
+    st.link_button("Reference: Atmotube.com",
+                   "https://atmotube.com/atmocube-support/indoor-air-quality-index-iaqi#:~:text=To"
+                   "%20calculate%20the%20AQI%2C%20the%20EPA%20measured%20outdoor,"
+                   "individual%20index%20based%20on%20the%20EPA%E2%80%99s%20breakpoint%20table.")
+
+    st.write("Parameter yang terdapat dalam rumus di atas adalah sebagai berikut:")
+    st.write("IAQI = Indeks Kualitas Udara (Air Quality Index)")
+    st.write("CO2 = Konsentrasi CO2 dalam udara")
+    st.write("TVOC = Konsentrasi TVOC dalam udara")
+    st.write("I_H = Nilai IAQI tertinggi dalam rentang konsentrasi CO2 atau TVOC")
+    st.write("I_L = Nilai IAQI terendah dalam rentang konsentrasi CO2 atau TVOC")
+    st.write("C_H = Konsentrasi CO2 atau TVOC tertinggi dalam rentang konsentrasi CO2 atau TVOC")
+    st.write("C_L = Konsentrasi CO2 atau TVOC terendah dalam rentang konsentrasi CO2 atau TVOC")
+
+    df_filtered["iaqi_co2"], df_filtered["iaqi_tvoc"] = zip(
+        *df_filtered.apply(lambda x: calculate_iaqi(x["co2"], x["tvoc"]), axis=1))
+
+    # After IAQI Calculation make a new column that show category of IAQI Score of Good, Moderate, and Hazardous
+
+    def iaqi_category(iaqi):
+        if 0 <= iaqi <= 50:
+            return "Good"
+        elif 51 <= iaqi <= 100:
+            return "Moderate"
+        else:
+            return "Hazardous"
+
+    df_filtered["iaqi_category_co2"] = df_filtered["iaqi_co2"].apply(iaqi_category)
+    df_filtered["iaqi_category_tvoc"] = df_filtered["iaqi_tvoc"].apply(iaqi_category)
+
+    st.dataframe(df_filtered)
+
+    # Buat sebuah plot yang menunjukkan IAQI category dari CO2 yang sudah dipisah menjadi 3 line yaitu hijau untuk Good,
+    # kuning untuk Moderate, dan merah untuk Hazardous. dan y juga berdasarkan category yang sudah diubah menjadi
+    # kategori Good = 1, Moderate = 0, dan Hazardous = -1 dan x nya adalah waktu setiap 5 menit
+
+    fig = go.Figure()
+
+    for category in df_filtered["iaqi_category_co2"].unique():
+        df_category = df_filtered[df_filtered["iaqi_category_co2"] == category]
+        if category == "Good":
+            color = "green"
+        elif category == "Moderate":
+            color = "yellow"
+        else:
+            color = "red"
+        fig.add_trace(go.Scatter(y=[1] * len(df_category), mode="markers", marker=dict(color=color), name=category))
+
+    fig.update_layout(title="IAQI Category CO2", xaxis_title="Time", yaxis_title="Category", showlegend=True)
+    st.plotly_chart(fig)
+
+    st.header("Data Preprocessing")
+
+    # Splitting the dataset into training and testing set
+
+    X = df_filtered[["co2", "tvoc"]].values
+    y = df_filtered[["iaqi_category_co2"]].values
+
+    y = pd.DataFrame(y).replace({"Good": 1, "Moderate": 2, "Hazardous": 3}).values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    st.write("X_train shape:", X_train.shape)
+
+    st.write("X_test shape:", X_test.shape)
+
+    st.write("y_train:", y_train)
+
+    st.header("Modeling")
+
+    # Create a model using LSTM classification
+
+    model = Sequential()
+    model.add(LSTM(50, activation="relu", input_shape=(X_train.shape[1], 1)))
+    model.add(Dense(1))
+    # Bisakah kamu jelaskan kenapa menggunakan activation relu?
+    # Jawaban: Karena activation relu merupakan activation function yang paling sering digunakan dan memiliki performa
+    # yang baik dalam model deep learning. Activation relu juga memiliki kelebihan yaitu mengatasi masalah vanishing
+    # gradient yang sering terjadi pada activation function sigmoid dan tanh.
+
+    model.compile(optimizer="adam", loss="mse")
+    # Bisakah kamu jelaskan kenapa menggunakan loss mse?
+    # Jawaban: Karena loss mse merupakan loss function yang paling sering digunakan untuk regression problem dan
+    # memiliki performa yang baik dalam model deep learning. Loss mse juga memiliki kelebihan yaitu menghitung
+    # error antara prediksi dan target dengan cara menghitung squared error.
+    # Bisakah kamu jelaskan kenapa menggunakan optimizer adam?
+    # Jawaban: Karena optimizer adam merupakan optimizer yang paling sering digunakan dan memiliki performa yang baik
+    # dalam model deep learning. Optimizer adam juga memiliki kelebihan yaitu menghitung learning rate secara adaptif
+    # dan menghitung momentum secara adaptif.
+
+    # Fit the model
+
+    model.fit(X_train, y_train, epochs=200, batch_size=32, verbose=1)
+
+    # Evaluate the model
+
+    y_pred = model.predict(X_test)
+
+    st.write("y_pred:", y_pred)
+
+    # Konversi prediksi menjadi kategori
+    # menggunakan np.where untuk check kondisi jika prediksi kurang dari atau sama dengan 1, maka kategori adalah 1,
+    # jika prediksi kurang dari atau sama dengan 2, maka kategori adalah 2, jika tidak keduanya,
+    # maka kategori adalah 3
+    y_pred_category = np.where(y_pred <= 1, 1, np.where(y_pred <= 2, 2, 3))
+
+    st.write("y_pred_category:", y_pred_category)
+
+    # Evaluate the model
+
+    accuracy = accuracy_score(y_test, y_pred_category)
+
+    st.write("Accuracy:", accuracy)
+
+    # Print MSE
+
+    mse = mean_squared_error(y_test, y_pred_category)
+
+    st.write("MSE:", mse)
+
+    # Compare the actual and prediction
+
+    df_compare = pd.DataFrame({"Actual": y_test.flatten(), "Prediction": y_pred_category.flatten()})
+
+    df_compare["Accuracy"] = np.where(df_compare["Actual"] == df_compare["Prediction"], "Accurate", "Not Accurate")
+
+    df_compare["Actual"] = pd.DataFrame(df_compare["Actual"]).replace({1: "Good", 2: "Moderate", 3: "Hazardous"})
+
+    df_compare["Prediction"] = pd.DataFrame(df_compare["Prediction"]).replace({1: "Good", 2: "Moderate", 3: "Hazardous"})
+
+    st.dataframe(df_compare)
+
+    # Save the model
+
+    model.save("model.h5")
+
+    st.write("Model has been saved as model.h5")
 
 
 page_names_to_funcs = {
